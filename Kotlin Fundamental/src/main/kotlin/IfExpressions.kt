54cb9eac1c3ec7aae59aba860b144a56f5f13317
AnggunWardaniM <69030456+AnggunWardaniM@users.noreply.github.com>
2020-08-11 21:40:41 +0700

    Update and rename ItExpressions.kt to IfExpressions.kt

---
+++ Kotlin Fundamental/src/main/kotlin/IfExpressions.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val nilai = 80
+    val kkm = 75
+    val hasil : String
+    hasil = if (nilai > kkm){
+        "Nilai kamu $nilai, Selamat Ya!!"
+    }else {
+        "Nilai kamu $kkm, Silahkan ikut remidial"
+    }
+    print(hasil)
+}
